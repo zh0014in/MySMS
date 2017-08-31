ad730ff53235ad1a6916007895ef04415c38a04a
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2017-08-31 15:28:15 +0800

    first add

---
+++ app/src/main/java/lll/zzz/mysms/MainActivity.java
@@ -0,0 +1,98 @@
+package lll.zzz.mysms;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.*;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static android.R.attr.duration;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBtn(View v)
+    {
+    ExportSms();
+    }
+
+    private void ExportSms(){
+        // public static final String INBOX = "content://sms/inbox";
+// public static final String SENT = "content://sms/sent";
+// public static final String DRAFT = "content://sms/draft";
+        Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
+
+        if (cursor.moveToFirst()) { // must check the result to prevent exception
+            String msgData = "";
+            do {
+                for(int idx=0;idx<cursor.getColumnCount();idx++)
+                {
+                    msgData += " " + cursor.getColumnName(idx) + ":" + cursor.getString(idx);
+                }
+
+            } while (cursor.moveToNext());
+
+            // use msgData
+            if(isExternalStorageWritable()){
+                File folder = getSmsStorageDir("sms");
+                try {
+                    File file = new File(folder, "sms.txt");
+                    FileOutputStream stream = new FileOutputStream(file);
+                    try {
+                        stream.write(msgData.getBytes());
+                    } catch (IOException e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    } finally {
+                        Toast.makeText(getApplicationContext(), "exported", Toast.LENGTH_SHORT).show();
+                        stream.close();
+                    }
+                }catch (IOException e){
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        } else {
+            // empty box, no SMS
+        }
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    public File getSmsStorageDir(String smsFileName) {
+        // Get the directory for the user's public pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS), smsFileName);
+        if (!file.mkdirs()) {
+
+        }
+        return file;
+    }
+}
